081c40316e611d94519348fe42f3b7cefa2db172
PrathamNathani <73383584+PrathamNathani@users.noreply.github.com>
2021-12-29 16:29:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width:310,
+    height:300, 
+    image_format:"png",
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("results").innerHTML="<img id='capture_image' src='"+data_uri+"' />";
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+
+var classifier=ml5.imageClassifier("MobileNet",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+function identify_image(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotresult);          
+}
+function gotresult(error,results){
+    if(error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
